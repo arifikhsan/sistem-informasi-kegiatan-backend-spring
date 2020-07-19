fca0723357cec76de4118e321e90eed8dad41845
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-07-19 21:22:59 +0700

    initial commit

---
+++ src/main/java/sh/now/arifikhsanudin/sisteminformasikegiatanbackendspring/SistemInformasiKegiatanBackendSpringApplication.java
@@ -0,0 +1,13 @@
+package sh.now.arifikhsanudin.sisteminformasikegiatanbackendspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemInformasiKegiatanBackendSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemInformasiKegiatanBackendSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sh/now/arifikhsanudin/sisteminformasikegiatanbackendspring/SistemInformasiKegiatanBackendSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package sh.now.arifikhsanudin.sisteminformasikegiatanbackendspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemInformasiKegiatanBackendSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
